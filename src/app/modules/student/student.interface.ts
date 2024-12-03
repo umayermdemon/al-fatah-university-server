3dcd0cd86e99adab960c0dc98ae1dc8d7626640a
umayermdemon <mamudmdemon@gmail.com>
2024-12-03 17:03:39 +0600

    student interface & student model complete

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,36 @@
+export type TUserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TStudent = {
+  id: string;
+  name: TUserName;
+  gender: "male" | "female" | "other";
+  dateOfBirth: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImage: string;
+  isDeleted: boolean;
+};
---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  TUserName,
+} from "./student.interface";
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+const localGuardianSchema = new Schema<TLocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<TStudent>(
+  {
+    id: { type: String, required: true },
+    name: userNameSchema,
+    gender: ["male", "female", "other"],
+    dateOfBirth: { type: Date, required: true },
+    email: { type: String, required: true },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+    guardian: guardianSchema,
+    localGuardian: localGuardianSchema,
+    profileImage: { type: String, required: true },
+    isDeleted: { type: Boolean, required: true },
+  },
+  { timestamps: true },
+);
+
+export const Student = model<TStudent>("student", studentSchema);
