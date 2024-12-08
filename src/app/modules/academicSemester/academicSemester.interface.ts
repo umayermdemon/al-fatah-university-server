dd6946f34e58c61f693fb261650dc6b744582e57
umayermdemon <mamudmdemon@gmail.com>
2024-12-08 09:35:26 +0600

    acedemic semester added

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,21 @@
+type Month =
+  | "January"
+  | "February"
+  | "March"
+  | "April"
+  | "May"
+  | "June"
+  | "July"
+  | "August"
+  | "September"
+  | "October"
+  | "November"
+  | "December";
+
+export type TAcademicSemester = {
+  name: "Autumn" | "Summer" | "Fall";
+  code: "01" | "02" | "03";
+  year: Date;
+  startMonth: Month;
+  endMonth: Month;
+};
