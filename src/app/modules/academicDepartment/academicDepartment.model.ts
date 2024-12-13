f6eca0151af3fee81a0b77b503e85cdcdb4c3953
umayermdemon <mamudmdemon@gmail.com>
2024-12-13 07:38:53 +0600

    academic department  completed

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -14,11 +14,24 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
-// academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
-//   const query = this.getQuery;
-//   console.log(query);
-//   next();
-// });
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error("This department is already exists");
+  }
+  next();
+});
+
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error("The department doesn't exists!");
+  }
+  next();
+});
 
 export const AcademicDepartment = model<TAcademicDepartment>(
   "AcademicDepartment",
