95e2189565aea9f7054e8b7009053d542be6a778
MD EMON MIAH <142793133+umayermdemon@users.noreply.github.com>
2025-08-13 17:45:06 +0600

    Update academicDepartment.controller.ts

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -15,7 +15,7 @@ const createAcademicDepartment = catchAsync(async (req, res) => {
     data: result,
   });
 });
-// get all academic Department data
+// get all academic Department
 const getAllAcademicDepartment = catchAsync(async (req, res) => {
   const result =
     await AcademicDepartmentServices.getAllAcademicDepartmentFromDb();
